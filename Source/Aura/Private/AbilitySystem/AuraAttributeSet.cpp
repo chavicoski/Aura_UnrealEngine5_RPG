2d108f45670c651711e7c95feb46f03476e98b99
chavicoski <alvaro.lopez.chilet@gmail.com>
2023-10-15 20:30:06 +0200

    Added getters/setter for attribute system

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -5,6 +5,10 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
+	InitHealth(100.0f);
+	InitMaxHealth(100.0f);
+	InitMana(50.0f);
+	InitMaxMana(50.0f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
