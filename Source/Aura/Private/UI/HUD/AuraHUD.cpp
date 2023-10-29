9a2b1fdc455ae9767cb106b92beddc699922e2c8
chavicoski <alvaro.lopez.chilet@gmail.com>
2023-10-29 10:38:31 +0100

    Added HUD class

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,10 @@
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
